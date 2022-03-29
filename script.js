d11315380a3a7ae0fedd0413fb2e91e2b2bd78a5
Gabriel Mathias <gablemathias@icloud.com>
2022-03-29 15:51:51 -0300

    add costumers object

---
+++ script.js
@@ -0,0 +1,18 @@
+const list = document.querySelector('#list');
+
+const clients = [
+  {
+    name: "Joao",
+    email: "joao@test.com"
+  },
+  {
+    name: "Lucas",
+    email: "lucas@test.com"
+  },
+  {
+    name: "Saulo",
+    email: "lucas@test.com"
+  }
+];
+
+
